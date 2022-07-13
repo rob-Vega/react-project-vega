b11f3c924bc3f49a2ca708790dd1f6cb517c1d28
rob-Vega <rvegag95@gmail.com>
2022-07-12 20:40:50 -0400

    NavBar component created

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return (
+        <header>
+            <h1>Store 🐣</h1>
+            <ul>
+                <li>Home</li>
+                <li>Recovery</li>
+                <li>Support</li>
+                <li>Debuff</li>
+                <li>About Us</li>
+            </ul>
+        </header>
+    );
+}
+
+export default NavBar;
